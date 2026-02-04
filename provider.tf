d12914f891ad628c85cb9dcd18f4dae1b651e936
PrashantShevale <prashantshevale619@gmail.com>
2026-02-04 22:38:29 +0530

    Configure Terraform provider and backend settings

--- provider.tf
+++ provider.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.52.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-pasha"
+    storage_account_name = "storagepasha"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "3be50968-9f1c-40cd-9f07-cf3bfdb72e22"
 
+}
