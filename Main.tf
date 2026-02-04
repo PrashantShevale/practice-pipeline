11d5c7742c4121dd553b176909e8d8eba39186f5
PrashantShevale <prashantshevale619@gmail.com>
2026-02-04 22:36:18 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.58.0"
-    }
-  }
-}
-provider "azurerm" {
-  features {}
-  subscription_id = "3be50968-9f1c-40cd-9f07-cf3bfdb72e22"
-}
-
-
 resource "azurerm_resource_group" "rg" {
   name     = "new-pipeline-rg"
   location = "West Europe"
